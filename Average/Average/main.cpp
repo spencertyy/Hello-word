389bb0ccea72c6fd02a99a71860123c23eb752ea
spencertu <u1483557@umail.utah.edu>
2023-08-22 12:36:57 -0600

    Group lab/Average

---
+++ Average/Average/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Average
+//
+//  Created by YuYao Tu on 8/22/23.
+//
+
+#include <iostream>
+using namespace std;
+int main(){
+  
+    float num1;
+    float num2;
+    float num3;
+    float num4;
+    float num5;
+float Average;
+
+    cout << "Enter 5 assignment scores:\n";
+    cin >> num1 >> num2 >> num3 >> num4 >> num5;
+    cout << " Average :" << (num1 + num2 + num3 + num4 + num5)/5 << endl;
+    
+    return 0;
+}
